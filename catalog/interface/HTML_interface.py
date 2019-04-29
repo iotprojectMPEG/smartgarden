23adfab0d4c33790448ccee93bcbad03edc9e273
Gennaro <gennaro.rende@yahoo.com>
2019-04-29 16:44:04 +0200

    Other functions added to HTML

--- catalog/interface/HTML_interface.py
+++ catalog/interface/HTML_interface.py
@@ -1,52 +1,53 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 import random
 import string
 import cherrypy
 
 
-class StringGenerator(object):
+class ItemAdder(object):
 
     @cherrypy.expose
-    def index1(self):
+    def index(self):
         return """<html>
           <head></head>
           <body>
-
             <h2>ADD GARDEN</h2>
-
-            <form action="generate">
+            <form method="get" action="posting" target="_blank">
               Name:<br>
-              <input type="text" name="Name" value="">
+              <input type="text" name="name" value="">
               <br>
               Location:<br>
-              <input type="text" name="Location" value="">
+              <input type="text" name="location" value="">
               <br><br>
               <input type="submit" value="Submit">
-            </form>
-
-
-           </body>
-        </html>"""
-
-
-
-    @cherrypy.expose
-    def index2(self):
-        return """<html>
-          <head></head>
-          <body>
-            <form method="get" action="generate">
-              <input type="text" value="8" name="length" />
-              <button type="submit">Give it now!</button>
             </form>
           </body>
         </html>"""
 
-
     @cherrypy.expose
-    def generate(self, length=8):
-        return ''.join(random.sample(string.hexdigits, int(length)))
+    def posting(self, name=None, location=None):
+        #prendere input e metterli nel catalog
+        return "ciao, %s" %name
+
+
+
+    # @cherrypy.expose
+    # def index2(self):
+    #     return """<html>
+    #       <head></head>
+    #       <body>
+    #         <form method="get" action="generate">
+    #           <input type="text" value="8" name="length" />
+    #           <button type="submit">Give it now!</button>
+    #         </form>
+    #       </body>
+    #     </html>"""
+    #
+    #
+    # @cherrypy.expose
+    # def generate(self, length=8):
+    #     return ''.join(random.sample(string.hexdigits, int(length)))
 
 
 if __name__ == '__main__':
-    cherrypy.quickstart(StringGenerator())
+    cherrypy.quickstart(ItemAdder())
