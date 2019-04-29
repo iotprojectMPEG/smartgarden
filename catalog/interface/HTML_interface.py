27f6415d7ac176a857413ab95ff95f415c5e54b0
Gennaro <gennaro.rende@yahoo.com>
2019-04-29 14:12:05 +0200

    first structure for the HTML form

---
+++ catalog/interface/HTML_interface.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import random
+import string
+import cherrypy
+
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+          <head></head>
+          <body>
+            <form method="get" action="generate">
+              <input type="text" value="8" name="length" />
+              <button type="submit">Give it now!</button>
+            </form>
+          </body>
+        </html>"""
+
+    @cherrypy.expose
+    def generate(self, length=8):
+        return ''.join(random.sample(string.hexdigits, int(length)))
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(StringGenerator())
