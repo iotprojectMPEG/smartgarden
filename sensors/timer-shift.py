ef4c872935357fea980c798f419ef8c36782908d
paolo-26 <paolo.1995@ymail.com>
2019-05-08 18:28:17 +0200

    Add file

---
+++ sensors/timer-shift.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Activates sensors in specific moments of the day.
+"""
+
+import json
+import requests
+import schedule
+
+FILENAME = "conf.json"
+
+def main():
+
+    with open(FILENAME, "r") as f:
+        conf = json.loads(f.read())
+
+    url = conf["catalogURL"]
+    port = conf["port"]
+
+    string = "http://" + url + ":" + port + "/static"
+    static = json.loads(requests.get(string).text)
+
+    for g in static["gardens"]:
+        hours = g["hours"]
+
+
+
+
+if __name__ == '__main__':
+    main()
