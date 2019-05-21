1edf664211f3b2877c7cac00301e5484288ee2d5
paolo-26 <paolo.1995@ymail.com>
2019-05-21 18:01:34 +0200

    Add calling function

--- sensors/timer-shift.py
+++ sensors/timer-shift.py
@@ -4,17 +4,28 @@
 Activates sensors in specific moments of the day.
 """
 
+import dht11 as dht
+import wind
+
 import json
 import requests
 import schedule
 import time
+import sys
 
 FILENAME = "conf.json"
+SEC = 10
 
-def call_sensors():
-    #Call sensors
-    print("Calling...")
-    pass
+def call_sensors(devices):
+    # Call sensors.
+
+    for dev in devices:
+        d = dev[0:dev.find('_')]
+        try:
+            func = d + '.get_data()'
+            print(eval(func))
+        except:
+            print("%s is not available!" % d)
 
 def main():
 
@@ -25,18 +36,33 @@ def main():
     port = conf["port"]
 
     string = "http://" + url + ":" + port + "/static"
-    static = json.loads(requests.get(string).text)
+
+    connected = 0
+    while connected == 0:
+        try:
+            static = json.loads(requests.get(string).text)
+            connected = 1
+            print("Service started")
+        except:
+            print("The catalog (%s) is not reachable!" % string)
+            print("Retrying in %d seconds..." % SEC)
+            time.sleep(SEC)
 
     for g in static["gardens"]:
-        hours = g["hours"]
+        for p in g["plants"]:
+            hours = p["hours"]
+            devices = []
+            for d in p["devices"]:
+                devices.append(d["devID"])
 
-    # Schedule times.
-    for h in hours:
-        schedule.every().day.at(h).do(call_sensors())
+            #hours = ['17:46']
+            for h in hours:
+                schedule.every().day.at(h).do(call_sensors, devices)
+                print("Schedule: %s > %s > %s" %(p["plantID"], h, devices))
 
-while True:
-    schedule.run_pending()
-    time.sleep(60)
+    while True:
+        schedule.run_pending()
+        time.sleep(3)
 
 if __name__ == '__main__':
     main()
