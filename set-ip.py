ef94d5a3cbaddc6405d9f1f8424e8d9db01467e6
paolo-26 <paolo.1995@ymail.com>
2019-05-25 12:30:28 +0200

    Add file

---
+++ set-ip.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script lets you change all IPs in json files.
+"""
+import json
+import sys, os
+
+
+def update_json(filename, key, value):
+
+    with open(filename, 'r') as fr:
+        data = json.loads(fr.read())
+
+    data[key] = value
+
+    with open(filename, 'w') as fw:
+        json.dump(data, fw, indent=2)
+
+
+def main():
+    ip = input("Type catalog IP followed by [Enter]:\n")
+    update_json("./thingspeak/conf.json", "catalogURL", ip)
+    update_json("./sensors/conf.json", "catalogURL", ip)
+    update_json("./sensors/dht11/conf.json", "catalogURL", ip)
+    update_json("./sensors/irrigator/conf.json", "catalogURL", ip)
+    update_json("./sensors/light/conf.json", "catalogURL", ip)
+    update_json("./sensors/rain/conf.json", "catalogURL", ip)
+    update_json("./sensors/wind/conf.json", "catalogURL", ip)
+
+    print("Success!")
+
+
+if __name__ == '__main__':
+    main()
