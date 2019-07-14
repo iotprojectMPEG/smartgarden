fe84da2f08c350052588bb1366d8f2e33b48b1e7
paolo-26 <paolo.1995@ymail.com>
2019-07-14 12:52:29 +0200

    Add publishing function

--- sensors/irrigator/irrigator.py
+++ sensors/irrigator/irrigator.py
@@ -9,6 +9,7 @@ import requests
 import threading
 import paho.mqtt.client as PahoMQTT
 import time
+import schedule
 
 import os, sys, inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -17,16 +18,22 @@ sys.path.insert(0, parent_dir)
 import updater
 
 FILENAME = "conf.json"
+BT = None
+
 
 def irrigate(id, time=300):
-    print("Starting irrigation on %s" % id)
+    """Dummy function which simulates irrigation hardware."""
+    print("Starting irrigation on %s for %s seconds" % (id, time))
+
 
 class PublisherSubscriber:
-    def __init__(self, clientID, topic, serverIP, port):
+    """Receives irrigation commands and publish irrigation values."""
+    def __init__(self, clientID, topic, serverIP, port, devID):
         self.clientID = clientID
+        self.devID = devID
         self.topic = topic
         self.messageBroker = serverIP
-        self.port = port
+        self.port = int(port)
         self._paho_mqtt = PahoMQTT.Client(clientID, False)
         self._paho_mqtt.on_connect = self.my_on_connect
         self._paho_mqtt.on_message = self.my_on_message_received
@@ -36,6 +43,7 @@ class PublisherSubscriber:
         self._paho_mqtt.connect(self.messageBroker, self.port)
         self._paho_mqtt.loop_start()
         self._paho_mqtt.subscribe(self.topic, 2)
+        print("Subscribed to %s" % self.topic)
 
     def stop(self):
         self._paho_mqtt.unsubscribe(self.topic)
@@ -43,21 +51,49 @@ class PublisherSubscriber:
         self._paho_mqtt.disconnect()
 
     def my_on_connect(self, client, userdata, flags, rc):
-        print ("Connected to %s - Result code: %d" % (self.messageBroker, rc))
+        print ("PS - Connected to %s - Result code: %d" % (self.messageBroker, rc))
         self.loop_flag = 0
 
     def my_on_message_received(self, client, userdata, msg):
+        """When a message is received, it takes the duration and calls the
+        publishing function. Alive messages are ignored.
+        """
         msg.payload = msg.payload.decode("utf-8")
         message = json.loads(msg.payload)
-        dev = message["devID"]
-        try:
-            duration = message["duration"]
-            irrigate(dev, duration)
-        except:
-            irrigate(dev)
+        dev = self.devID
+        for e in message["e"]:
+            if e["n"] == 'alive':
+                # print("Alive message")
+                pass
+            else:
+                try:
+                    duration = e["d"]
+                    irrigate(dev, duration)
+                    self.my_publish(duration, 1)
+                except:
+                    pass
+
+    def my_publish(self, duration, bool):
+        """Create a json which is then published via MQTT.
+        This will be collected by thingspeak.py and published on ThingSpeak
+        """
+        timestamp = round(time.time()) - BT
+        message = {
+        "bn": self.devID,
+        "bt": BT,
+        "e":
+           [{"n": "irrigation",
+            "u": "boolean",
+            "t": timestamp,
+            "v": duration
+            }]
+        }
+        self._paho_mqtt.publish(self.topic, json.dumps(message), 2)
+        print(json.dumps(message, indent=2), "published on %s" % self.topic)
 
 
 class PubData(threading.Thread):
+    """Main thread for MQTT publisher and subscriber."""
     def __init__(self, ThreadID, name):
         threading.Thread.__init__(self)
         self.ThreadID = ThreadID
@@ -77,20 +113,22 @@ class PubData(threading.Thread):
     def run(self):
         print("Topics:", self.topic)
         pubsub = PublisherSubscriber(self.devID + '_1', self.topic[0], self.broker_ip,
-                          int(self.mqtt_port))
+                          int(self.mqtt_port), self.devID)
         pubsub.start()
-
         while pubsub.loop_flag:
             print("Waiting for connection...")
             time.sleep(1)
 
         while True:
-            time.sleep(60)
+            time.sleep(1)
 
         pubsub.stop()
 
 
 def main():
+    global BT
+    BT = round(time.time())
+
     thread1 = updater.Alive(1, "Alive")
     thread2 = PubData(2, "PubData")
 
