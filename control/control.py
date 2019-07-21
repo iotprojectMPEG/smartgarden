efc865eb462652fa886b9d8b7446bf1f70228f3a
paolo-26 <paolo.1995@ymail.com>
2019-07-21 20:42:09 +0200

    Add updating function

--- control/control.py
+++ control/control.py
@@ -5,3 +5,58 @@ This script...
 """
 import json
 import sys, os
+import threading
+import requests
+import time
+
+FILE1 = "conf.json"
+LIST = []
+
+
+def read_file(filename):
+    """Read json file to get catalogURL, port and gardenID."""
+    with open(filename, "r") as f:
+        data = json.loads(f.read())
+        url = data["catalogURL"]
+        port = data["port"]
+        gardenID = data["gardenID"]
+        return (url, port, gardenID)
+
+
+
+
+class UpdateList(threading.Thread):
+    """Updates global list of plants every day. If the list is the same as
+    before, it does not update it.
+    """
+    def __init__(self, ThreadID, name):
+        threading.Thread.__init__(self)
+        self.ThreadID = ThreadID
+        self.name = name
+
+    def run(self):
+        global LIST
+        while True:
+            new_list = []
+            url, port, gardenID = read_file(FILE1)
+            string = "http://" + url + ":" + port + "/static"
+            data = json.loads(requests.get(string).text)
+            for g in data["gardens"]:
+                if g["gardenID"] == gardenID:
+                    for p in g["plants"]:
+                        new_list.append(p["plantID"])
+
+            if LIST != new_list:
+                LIST = new_list
+                print("New list: ", LIST)
+            else:
+                print("List is up to date.")
+
+            time.sleep(86400)
+
+def main():
+    upd = UpdateList(1, "UpdateList")
+    upd.start()
+
+if __name__ == '__main__':
+    main()
