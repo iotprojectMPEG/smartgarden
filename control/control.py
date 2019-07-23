189ef939530a88ed41347d6869326bddbbde6b8e
paolo-26 <paolo.1995@ymail.com>
2019-07-23 10:47:27 +0200

    Add post_mod function

--- control/control.py
+++ control/control.py
@@ -12,13 +12,14 @@ import datetime
 import schedule
 from rain_control import rain_control
 from light_control import light_control
-
+from wind_control import wind_control
+from hum_control import hum_control
+from temp_control import temp_control
 
 FILE1 = "conf.json"
-LIST = []
-
-#
-# def write_catalog(filename):
+LIST = []  # Plant list
+URL = None  # Catalog URL
+PORT = None  # Catalog port
 
 
 def read_file(filename):
@@ -28,33 +29,55 @@ def read_file(filename):
         url = data["catalogURL"]
         port = data["port"]
         gardenID = data["gardenID"]
+
+        global URL
+        URL = url
+        global PORT
+        PORT = port
+
         return (url, port, gardenID)
 
 
-def rain(plantID):
+def post_mod(plantID, h, mod):
+    data = {
+        "plantID": plantID,
+        "hour": h,
+        "mod": mod
+    }
+    string = "http://" + URL + ":" +str(PORT) + "/hours"
+
+    # Write on catalog.
+    print(string)
+    print(json.dumps(data))
+    r = requests.post(string, data=json.dumps(data))
+
+
+def rain(plantID, h, type):
     if rain_control.get_result(plantID):
-        # Write on catalog.
         pass
 
-def light(plantID):
+
+def light(plantID, h, type):
+    print(plantID, type)
     sec = rain_control.get_result(plantID)
-    # Write on catalog.
     pass
 
-def wind(plantID):
+
+def wind(plantID, h, type):
     sec = wind_control.get_result(plantID)
-    # Write on catalog.
+    post_mod(plantID, h, sec)
     pass
 
-def hum(plantID):
+
+def hum(plantID, h, type):
     sec = hum_control.get_result(plantID)
-    # Write on catalog.
     pass
 
-def temp(plantID):
+
+def temp(plantID, h, type):
     sec = temp_control.get_result(plantID)
-    # Write on catalog.
     pass
+
 
 class Plant(threading.Thread):
     def __init__(self, ThreadID, name, list):
@@ -71,20 +94,20 @@ class Plant(threading.Thread):
                     for r in d["resources"]:
                         res = r["n"]
                         if res == 'rain':
-                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h))
-                            schedule.every().day.at(h).do(rain, p["plantID"])
+                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h["time"]))
+                            schedule.every().day.at(h["time"]).do(rain, p["plantID"], h["time"], h["type"])
                         if res == 'light':
-                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h))
-                            schedule.every().day.at(h).do(light, p["plantID"])
+                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h["time"]))
+                            schedule.every().day.at(h["time"]).do(light, p["plantID"], h["time"], h["type"])
                         if res == 'wind':
-                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h))
-                            schedule.every().day.at(h).do(wind, p["plantID"])
+                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h["time"]))
+                            schedule.every().day.at(h["time"]).do(wind, p["plantID"], h["time"], h["type"])
                         if res == 'humidity':
-                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h))
-                            schedule.every().day.at(h).do(hum, p["plantID"])
+                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h["time"]))
+                            schedule.every().day.at(h["time"]).do(hum, p["plantID"], h["time"], h["type"])
                         if res == 'temperature':
-                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h))
-                            schedule.every().day.at(h).do(temp, p["plantID"])
+                            print("Scheduling %s for %s at %s" % (res, p["plantID"], h["time"]))
+                            schedule.every().day.at(h["time"]).do(temp, p["plantID"], h["time"], h["type"])
 
         # Check schedules every 30 seconds.
         while True:
@@ -92,12 +115,7 @@ class Plant(threading.Thread):
             time.sleep(30)
 
 
-        # Keep on schedules.
-        #
-        # if 'rain' in self.resources:
-        #     do_not_irrigate = rain_control.get_result("p_1002")
-        #     print("Do NOT irrigate:", do_not_irrigate)
-        #
+        # To do later:
         # if 'light' in self.resources:
         #     sec = light_control.get_result()
         #     h = datetime.datetime.strptime(self.hours, '%H:%M')
@@ -105,9 +123,6 @@ class Plant(threading.Thread):
         #     h = format(h, '%H:%M')
         #     print(h)
 
-        ######################################################################
-        # TO DO: write modification on dynamic part of the catalog.
-        ######################################################################
 
 class UpdateList(threading.Thread):
     """Updates global list of plants every day. If the list is the same as
@@ -128,24 +143,21 @@ class UpdateList(threading.Thread):
             for g in data["gardens"]:
                 if g["gardenID"] == gardenID:
                     for p in g["plants"]:
-                        # new_list.append(p["plantID"])
                         new_list.append(p)
 
             if LIST != new_list:
                 LIST = new_list
-                print("New list!")#: ", LIST)
+                print("Updated list!")
 
-                # Stop thread
+                # Stop thread.
                 try:
                     planter.stop()
                 except:
                     pass
 
+                # (Re)start thread.
                 planter = Plant(101, "Planter", new_list)
                 planter.start()
-                # Restart thread
-
-
 
             else:
                 print("List is up to date.")
@@ -153,19 +165,10 @@ class UpdateList(threading.Thread):
             time.sleep(86400)
 
 
-def create_schedules():
-    pass
-
-
-def dummy_schedule(string):
-    print("Executed dummy schedule. String: ", string)
-
 def main():
     upd = UpdateList(1, "UpdateList")
     upd.start()
 
-    #plt = Plant(2, "Plant", "p_1001", "08:00")
-    #plt.start()
 
 if __name__ == '__main__':
     main()
