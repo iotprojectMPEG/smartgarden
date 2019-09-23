3520c939f93dfe24b89fc5db9ee9341186824457
paolo-26 <paolo.1995@ymail.com>
2019-09-23 16:34:46 +0200

    Add files

--- set-ip.py
+++ set-ip.py
@@ -61,12 +61,16 @@ def main():
     update_json("./telegram-bot/conf.json", "catalogURL", ip)
     update_json("./sensors/conf.json", "catalogURL", ip)
     update_json("./sensors/dht11/conf.json", "catalogURL", ip)
+    update_json("./sensors/dht11_2/conf.json", "catalogURL", ip)
     update_json("./sensors/irrigator/conf.json", "catalogURL", ip)
+    update_json("./sensors/irrigator_2/conf.json", "catalogURL", ip)
     update_json("./sensors/light/conf.json", "catalogURL", ip)
     update_json("./sensors/rain/conf.json", "catalogURL", ip)
     update_json("./sensors/wind/conf.json", "catalogURL", ip)
     update_json("./catalog/conf.json", "catalogURL", ip)
+    update_json("./thingspeak/conf.json", "catalogURL", ip)
     update_cherrypy("./catalog/cherrypyconf", "server.socket_host", ip)
+    update_cherrypy("./freeboard/conf", "server.socket_host", ip)
     update_json("./catalog/api.json", "telegramtoken", ip3)
 
 
