48bca67b14e857382c1e0436aec4a1e0cd7726f5
paolo-26 <paolo.1995@ymail.com>
2019-09-25 11:41:32 +0200

    Fix indentation

--- control/light_control/light_control.py
+++ control/light_control/light_control.py
@@ -31,70 +31,70 @@ def get_api(plantID):
     return readAPI, channel
 
 
-    def get_result(plantID, devID, type):
-        """Get the last entries on humidity field and decides if it is necessary or not
-        to adjust
-        """
-        readAPI, channelID = get_api(plantID)
+def get_result(plantID, devID, type):
+    """Get the last entries on humidity field and decides if it is necessary or not
+    to adjust
+    """
+    readAPI, channelID = get_api(plantID)
 
-        url, port = read_file(FILE)
-        string = "http://" + url + ":" + port + "/info/" + devID
-        r = json.loads(requests.get(string).text)
-        for i in r["resources"]:
-            if i["n"]=="light":
-                f=i["f"]
+    url, port = read_file(FILE)
+    string = "http://" + url + ":" + port + "/info/" + devID
+    r = json.loads(requests.get(string).text)
+    for i in r["resources"]:
+        if i["n"]=="light":
+            f=i["f"]
 
-        fieldID = str(f)
-        channelID = str(channelID)
-        readAPI = str(readAPI)
-        minutes = str(10)
-        string = ("https://api.thingspeak.com/channels/" + channelID + "/fields/" +
-                  fieldID + ".json?api_key=" + readAPI + "&minutes=" + minutes)
-        res = json.loads(requests.get(string).text)
-        data = []
-        for r in res["feeds"]:
-            try:
-                data.append(int(r["field"+str(f)])) #
-            except:
-                pass
+    fieldID = str(f)
+    channelID = str(channelID)
+    readAPI = str(readAPI)
+    minutes = str(10)
+    string = ("https://api.thingspeak.com/channels/" + channelID + "/fields/" +
+              fieldID + ".json?api_key=" + readAPI + "&minutes=" + minutes)
+    res = json.loads(requests.get(string).text)
+    data = []
+    for r in res["feeds"]:
+        try:
+            data.append(int(r["field"+str(f)])) #
+        except:
+            pass
 
-        if data != []:
-            m = np.mean(data)
-
-
-        if type ==  'evening':
-
-            if (m >= 100) and (m < 120):
-                return 1800
-
-            elif (m >= 120) and (m < 180):
-                return 3600
-
-            elif (m >= 180):
-                return 7200
-
-            elif (m >= 80 and m < 100):
-                return 0
-
-            elif (m < 80):
-                return -900
+    if data != []:
+        m = np.mean(data)
 
 
-        elif type == 'morning':
-            if (m >= 100) and (m < 120):
-                return -1800
+    if type ==  'evening':
 
-            elif (m >= 120) and (m < 180):
-                return -3600
+        if (m >= 100) and (m < 120):
+            return 1800
 
-            elif (m >= 180):
-                return -7200
+        elif (m >= 120) and (m < 180):
+            return 3600
 
-            elif (m >= 80 and m < 100):
-                return 0
+        elif (m >= 180):
+            return 7200
 
-            elif (m < 80):
-                return +900
+        elif (m >= 80 and m < 100):
+            return 0
+
+        elif (m < 80):
+            return -900
+
+
+    elif type == 'morning':
+        if (m >= 100) and (m < 120):
+            return -1800
+
+        elif (m >= 120) and (m < 180):
+            return -3600
+
+        elif (m >= 180):
+            return -7200
+
+        elif (m >= 80 and m < 100):
+            return 0
+
+        elif (m < 80):
+            return +900
 
 
 
