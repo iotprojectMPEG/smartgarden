592cbcaf6d282aedf6e122f835d945088b0a063e
paolo-26 <paolo.1995@ymail.com>
2019-11-30 12:50:43 +0100

    Create file

---
+++ control/p_1001/temp_control.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Tempearture control strategy.
+
+This script takes temp. records on ThingSpeak channels and decide how to edit
+irrigation parameters.
+"""
---
+++ control/p_1001/wind_control.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Wind control strategy.
+
+This script takes wind records on ThingSpeak channels and decide whether to
+irrigate or not.
+"""
