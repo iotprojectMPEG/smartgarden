24b9cf855450c8de8bf34e100040b8b0759d8b79
paolo-26 <paolo.1995@ymail.com>
2019-11-30 14:08:19 +0100

    Add scheduling to rain control

--- control/p_1001/rain_control.py
+++ control/p_1001/rain_control.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
+"""Rain control strategy.
+
 This script takes rain records on ThingSpeak channels and decide whether to
 irrigate or not.
 """
 import json
-import sys, os
 import requests
 import numpy as np
+import functions
+import time
+import threading
 
 FILE = "conf.json"
+TIME_LIST = []
 
 
 def read_file(filename):
@@ -22,10 +26,11 @@ def read_file(filename):
 
 
 def get_result(plantID, devID):
-    """Get the last entries on rain field and decides if it is necessary or not
+    """Get data from ThingSpeak and decide.
+
+    Get the last entries on rain field and decides if it is necessary or not
     to irrigate.
     """
-
     resource = "rain"
     time = "hours"
     tval = str(2)
@@ -49,7 +54,53 @@ def get_result(plantID, devID):
 
 
 def main():
-    print("Ex.1: add", get_result("p_1002", "d_1006"), "seconds")  # Example.
+    """Schedule and run strategy."""
+    url, port, plantID, devID, ts_url, ts_port = functions.read_file(FILE)
+    thread1 = SchedulingThread(1, "thread1")
+    thread1.start()
+
+    while True:
+        url, port, plantID, devID, ts_url, ts_port = functions.read_file(FILE)
+        string = ("http://" + url + ":" + port + "/info/" + plantID)
+        data = json.loads(requests.get(string).text)
+        hours = data["hours"]
+        env = data["environment"]
+
+        global TIME_LIST
+        for h in hours:
+            t = h["time"]
+            delayed_hour = functions.delay_h(t, -300)
+            entry = {
+                "hour": t,
+                "schedule_time": delayed_hour,
+                # "schedule_time": "15:43",
+                "env": env
+                }
+            TIME_LIST.append(entry)
+            print("Schedule: %s - %s" % (delayed_hour, plantID))
+
+        time.sleep(50)
+        TIME_LIST = []
+
+
+class SchedulingThread(threading.Thread):
+    """Scheduling thread to call strategy."""
+
+    def __init__(self, ThreadID, name):
+        """Initialise thread."""
+        threading.Thread.__init__(self)
+        self.ThreadID = ThreadID
+        self.name = name
+
+    def run(self):
+        """Run thread."""
+        global TIME_LIST
+        while True:
+            for e in TIME_LIST:
+                if e["schedule_time"] == time.strftime("%H:%M"):
+                    get_result(e["env"], e["hour"])
+                    TIME_LIST.remove(e)
+            time.sleep(3)
 
 
 if __name__ == '__main__':
