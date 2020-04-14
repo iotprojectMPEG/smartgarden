88b301aba2c1355d51ebb2513de172a2b8f1b16b
Gennaro Rende <gennarorende@MBP-di-Gennaro.lan>
2020-04-14 15:55:03 +0200

    time_control

---
+++ control/plant1/time_control.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Common functions to control strategies."""
+import datetime
+import json
+import requests
+
+
+def main():
+    """Schedule and run strategy."""
+    url, port, plantID, devID, ts_url, ts_port = functions.read_file(FILE)
+    thread1 = SchedulingThread(1, "thread1")
+    thread1.start()
+
+    while True:
+        url, port, plantID, devID, ts_url, ts_port = functions.read_file(FILE)
+        string = ("http://" + url + ":" + port + "/info/" + plantID)
+        data = json.loads(requests.get(string).text)
+        hours = data["hours"]
+        env = data["environment"]
+
+        global TIME_LIST
+        for h in hours:
+            t = h["time"]
+            delayed_hour = functions.delay_h(t, -300)
+            entry = {
+                "hour": t,
+                "schedule_time": delayed_hour,
+                # "schedule_time": "15:43",
+                "env": env
+                }
+            TIME_LIST.append(entry)  # Fill timetable.
+            print("Humidity check at: %s - %s" % (delayed_hour, plantID))
+
+        time.sleep(86400)  # One day
+        TIME_LIST = []  # Reset timetable
+
+
+class SchedulingThread(threading.Thread):
+    """Scheduling thread to call strategy."""
+
+    def __init__(self, ThreadID, name):
+        """Initialise thread."""
+        threading.Thread.__init__(self)
+        self.ThreadID = ThreadID
+        self.name = name
+
+    def run(self):
+        """Run thread.
+        Check if current hour correspond to one entry in the timetable, if so
+        call the strategy and remove the current hour from the timetable.
+        """
+        global TIME_LIST
+        while True:
+            for e in TIME_LIST:
+                if e["schedule_time"] == time.strftime("%H:%M"):
+                    get_result(e["env"], e["hour"])
+                    TIME_LIST.remove(e)
+            time.sleep(3)
+
+
+if __name__ == '__main__':
+    main()
